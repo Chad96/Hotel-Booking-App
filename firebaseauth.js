bc9cf8dee7ab6cfe1440e39561eb76b3d8bd91d8
Chad96 <chadrackndalamba@gmail.com>
2024-09-10 19:12:06 +0200

    firebase

---
+++ firebaseauth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKbkzs9X2hNHSQ1UvUSez8xynIFMmX62g",
+  authDomain: "hotel-booking-app-d2493.firebaseapp.com",
+  projectId: "hotel-booking-app-d2493",
+  storageBucket: "hotel-booking-app-d2493.appspot.com",
+  messagingSenderId: "850286590028",
+  appId: "1:850286590028:web:2c0900f30ebed591fd5ce9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AdminDashboard from "./pages/AdminDashboard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+
 const App = () => {
   return (
     <Router>
